58629eda5dfcbf726d8e0726acfa14f339aa054a
matthew.hilty <hilty.matthew@gmail.com>
2019-05-24 12:11:54 -0400

    snapshot 8

--- test/suites/SmartDecodeJson/Maybe.purs
+++ test/suites/SmartDecodeJson/Maybe.purs
@@ -15,42 +15,31 @@ import Test.Utils (assertEquivalence)
 suitex :: TestSuite
 suitex =
   suite "Maybe" do
---     test "#0" do
---       let value = {}
---       let result = smartDecodeJson $ encodeJson value
---       assertEquivalence result value
---     test "#1" do
---       let value = { a0: 0 }
---       let result = smartDecodeJson $ encodeJson value
---       assertEquivalence result value
---     test "#2" do
---       let value = { a0: Just 0 }
---       let result = smartDecodeJson $ encodeJson value
---       assertEquivalence result value
-
---     test "#3" do
---       let value = { a0: 0, a1: Just 1 }
---       let result = smartDecodeJson $ encodeJson value
---       assertEquivalence result value
-
-    test "#3" do
-      let
-          value :: { a0 :: Int, a1 :: Maybe Int }
-          value = { a0: 0, a1: Just 1 }
-      let
-          result :: Either String { a0 :: Int, a1 :: Maybe Int }
-          result = smartDecodeJson $ encodeJson value
+    test "#0" do
+      let value = {}
+      let result = smartDecodeJson $ encodeJson value
       assertEquivalence result value
-
---     test "#4" do
---       let value = { a0: Just 0, a1: 1 }
---       let result = smartDecodeJson $ encodeJson value
---       assertEquivalence result value
---     test "#5" do
---       let value = { a0: 0, a1: Just 1, a2: "2" }
---       let result = smartDecodeJson $ encodeJson value
---       assertEquivalence result value
---     test "#6" do
---       let value = { a0: 0, a1: Just 1, a2: "2", a3: Just "3" }
---       let result = smartDecodeJson $ encodeJson value
---       assertEquivalence result value
+    test "#1" do
+      let value = { a0: 0 }
+      let result = smartDecodeJson $ encodeJson value
+      assertEquivalence result value
+    test "#2" do
+      let value = { a0: Just 0 }
+      let result = smartDecodeJson $ encodeJson value
+      assertEquivalence result value
+    test "#3" do
+      let value = { a0: 0, a1: Just 1 }
+      let result = smartDecodeJson $ encodeJson value
+      assertEquivalence result value
+    test "#4" do
+      let value = { a0: Just 0, a1: 1 }
+      let result = smartDecodeJson $ encodeJson value
+      assertEquivalence result value
+    test "#5" do
+      let value = { a0: 0, a1: Just 1, a2: "2" }
+      let result = smartDecodeJson $ encodeJson value
+      assertEquivalence result value
+    test "#6" do
+      let value = { a0: 0, a1: Just 1, a2: "2", a3: Just "3" }
+      let result = smartDecodeJson $ encodeJson value
+      assertEquivalence result value
