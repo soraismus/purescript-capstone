b1a850621c4d16439c31bdb820c10769d3de548c
matthew.hilty <hilty.matthew@gmail.com>
2019-05-26 00:27:21 -0400

    Add convenience-juncture modules.

---
+++ src/Data/Argonaut/Decode/Record/Cross2.purs
@@ -0,0 +1,5 @@
+module Data.Argonaut.Decode.Record.Cross2
+  ( module Data.Argonaut.Decode.Record.Cross2.Utils
+  ) where
+
+import Data.Argonaut.Decode.Record.Cross2.Utils (decodeJsonWith, decodeJsonWith')
---
+++ src/Data/Argonaut/Decode/Record/Override2.purs
@@ -0,0 +1,5 @@
+module Data.Argonaut.Decode.Record.Override2
+  ( module Data.Argonaut.Decode.Record.Override2.Utils
+  ) where
+
+import Data.Argonaut.Decode.Record.Override2.Utils (decodeJsonWith, decodeJsonWith')
