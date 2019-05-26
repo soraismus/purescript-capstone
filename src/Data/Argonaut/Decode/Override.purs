9bfed75269cbdd237f4169fd1432eb78dc96d3bf
matthew.hilty <hilty.matthew@gmail.com>
2019-05-25 21:14:21 -0400

    Add capacity to override field decoding.

---
+++ src/Data/Argonaut/Decode/Override.purs
@@ -0,0 +1,5 @@
+module Data.Argonaut.Decode.Override
+  ( module Data.Argonaut.Decode.Override.Utils
+  ) where
+
+import Data.Argonaut.Decode.Override.Utils (decodeJsonWith, decodeJsonWith')
---
+++ src/Data/Argonaut/Decode/Override/Class.purs
@@ -0,0 +1,95 @@
+module Data.Argonaut.Decode.Override.Class
+  ( class DecodeJsonWith
+  , decodeJsonWith
+  ) where
+
+import Prelude (class Bind, bind, ($))
+
+import Data.Argonaut.Core (Json)
+import Data.Argonaut.Decode.Cases (class Cases)
+import Data.Argonaut.Utils (getMissingFieldErrorMessage)
+import Data.Maybe (Maybe(Just, Nothing))
+import Data.Status (class Status, report, reportError)
+import Data.Symbol (class IsSymbol, SProxy(SProxy), reflectSymbol)
+import Foreign.Object (Object, lookup)
+import Record (get, insert)
+import Type.Data.RowList (RLProxy(RLProxy)) -- Argonaut dependency
+import Type.Equality (class TypeEquals, to)
+import Type.Row
+  ( class Cons
+  , class Lacks
+  , class RowToList
+  , Cons
+  , Nil
+  , kind RowList
+  )
+import Unsafe.Coerce (unsafeCoerce)
+
+class DecodeJsonWith
+  (f :: Type -> Type)
+  (l1 :: RowList)
+  (r1 :: # Type)
+  (l0 :: RowList)
+  (r0 :: # Type)
+  | l1 -> r1 l0 r0 where
+  decodeJsonWith
+    :: RLProxy l0
+    -> RLProxy l1
+    -> Record r1
+    -> Object Json
+    -> f (Record r0)
+
+instance decodeJsonWithNil
+  :: Status f
+  => DecodeJsonWith f Nil () Nil () where
+  decodeJsonWith _ _ _ _ = report {}
+
+instance decodeJsonWithCons
+  :: ( Bind f
+     , Cases dl r
+     , Cases dl' r'
+     , Cons s v r' r
+     , Cons s dv dr' dr
+     , DecodeJsonWith f dl' dr' l' r'
+     , IsSymbol s
+     , Lacks s r'
+     , Lacks s dr'
+     , RowToList r l
+     , RowToList r' l'
+     , RowToList dr dl
+     , RowToList dr' dl'
+     , Status f
+     , TypeEquals dv (Json -> f v)
+     )
+  => DecodeJsonWith f (Cons s dv dl') dr (Cons s v l') r
+  where
+  decodeJsonWith _ _ decoderRecord object = do
+    let
+      sProxy :: SProxy s
+      sProxy = SProxy
+
+      fieldName :: String
+      fieldName = reflectSymbol sProxy
+
+      decoder :: Json -> f v
+      decoder = to $ get sProxy decoderRecord
+
+      -- To prevent unnecessary creation of intermediate decoder records,
+      -- coercion is used rather than calling `Record.delete sProxy`
+      -- to induce the next expected type.
+      decoderRecord' :: Record dr'
+      decoderRecord' = unsafeCoerce decoderRecord
+
+    rest <-
+      decodeJsonWith
+        (RLProxy :: RLProxy l')
+        (RLProxy :: RLProxy dl')
+        decoderRecord'
+        object
+
+    case lookup fieldName object of
+      Just jsonVal -> do
+        val <- decoder jsonVal
+        report $ insert sProxy val rest
+      Nothing ->
+        reportError $ getMissingFieldErrorMessage fieldName
