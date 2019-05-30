c213debd67734559aef417e54ff6dd07d5ec18d6
matthew.hilty <hilty.matthew@gmail.com>
2019-05-30 17:41:33 -0400

    Remove Cross2.

--- src/Data/Argonaut/Decode/Record/Cross2.purs
+++
@@ -1,7 +0,0 @@
-module Data.Argonaut.Decode.Record.Cross2
-  ( module Data.Argonaut.Decode.Record.Cross2.Class
-  , module Data.Argonaut.Decode.Record.Cross2.Utils
-  ) where
-
-import Data.Argonaut.Decode.Record.Cross2.Class (RenameField(RenameField))
-import Data.Argonaut.Decode.Record.Cross2.Utils (decodeJsonWith, decodeJsonWith')
