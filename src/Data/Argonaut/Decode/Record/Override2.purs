8f5cf4207db42efef4ca6548889e2c59add37a5d
matthew.hilty <hilty.matthew@gmail.com>
2019-05-30 17:42:56 -0400

    Remove Override2.

--- src/Data/Argonaut/Decode/Record/Override2.purs
+++
@@ -1,5 +0,0 @@
-module Data.Argonaut.Decode.Record.Override2
-  ( module Data.Argonaut.Decode.Record.Override2.Utils
-  ) where
-
-import Data.Argonaut.Decode.Record.Override2.Utils (decodeJsonWith, decodeJsonWith')
