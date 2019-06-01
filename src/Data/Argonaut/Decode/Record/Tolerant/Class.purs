e22d13dcb5635287ea66d0a4f15ac842ba90a384
matthew.hilty <hilty.matthew@gmail.com>
2019-06-01 18:48:09 -0400

    snapshot

--- src/Data/Argonaut/Decode/Record/Tolerant/Class.purs
+++ src/Data/Argonaut/Decode/Record/Tolerant/Class.purs
@@ -9,11 +9,7 @@ import Prelude (bind, ($))
 
 import Control.Plus (class Plus, empty)
 import Data.Argonaut.Core (Json, toObject)
-import Data.Argonaut.Decode.Class
-  ( class DecodeJson
-  , class GDecodeJson
-  , decodeJson
-  ) as D
+import Data.Argonaut.Decode.Class (class DecodeJson, decodeJson) as D
 import Data.Argonaut.Decode.Record.Utils
   ( getMissingFieldErrorMessage
   , singleton
@@ -35,7 +31,7 @@ import Type.Row
   , kind RowList
   )
 
-class D.DecodeJson a <= DecodeJson a where
+class DecodeJson a where
   decodeJson :: Json -> Either String a
 
 instance decodeRecord
@@ -58,7 +54,7 @@ instance decodeRecord
 else instance decodeDecodeJson :: D.DecodeJson a => DecodeJson a where
   decodeJson = D.decodeJson
 
-class D.GDecodeJson r2 l2 <= GDecodeJson
+class GDecodeJson
   (l0 :: RowList)
   (l1 :: RowList)
   (l2 :: RowList)
@@ -124,7 +120,6 @@ instance gDecodeJson_ConsMany_Plus
   :: ( Cons s (f v) r1' r1
      , Cons s (f v) r2' r2
      , Cons s2' v2' r2'' r2'
-     , D.GDecodeJson r2'' l2''
      , GDecodeJson l0 l1' (Cons s2' v2' l2'') r2'
      , IsSymbol s
      , IsSymbol s2'
@@ -159,7 +154,6 @@ else instance gDecodeJson_ConsMany_nonPlus
   :: ( Cons s v r0' r0
      , Cons s v r2' r2
      , Cons s2' v2' r2'' r2'
-     , D.GDecodeJson r2'' l2''
      , GDecodeJson l0' l1 (Cons s2' v2' l2'') r2'
      , IsSymbol s
      , IsSymbol s2'
