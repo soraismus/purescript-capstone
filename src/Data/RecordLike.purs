cefdbed335730fbefb93f077ad785a65e1585369
matthew.hilty <hilty.matthew@gmail.com>
2019-06-02 13:22:15 -0400

    snapshot

--- src/Data/RecordLike.purs
+++ src/Data/RecordLike.purs
@@ -10,6 +10,7 @@ module Data.RecordLike
   , class RNub
   , class RRename
   , class RSet
+  , class RSingleton
   , class RUnion
   , rget
   , rdelete
@@ -22,6 +23,7 @@ module Data.RecordLike
   , rnub
   , rrename
   , rset
+  , rsingleton
   , runion
   ) where
 
@@ -33,25 +35,29 @@ import Data.Variant (inj, on) as Variant
 import Data.Variant.Internal (class VariantTags)
 import Record
   ( class EqualFields
-  , get
-  , set
-  , modify
-  , insert
   , delete
-  , rename
-  , equal
-  , merge
-  , union
   , disjointUnion
+  , get
+  , equal
+  , insert
+  , merge
+  , modify
   , nub
+  , rename
+  , set
+  , union
   ) as Record
+import Record.Extra.Utils (singleton) as Record
 import Type.Data.RowList (RLProxy) -- Argonaut dependency
 import Type.Row
   ( class Cons
   , class Lacks
+  , class ListToRow
   , class Nub
   , class RowToList
   , class Union
+  , Cons
+  , Nil
   , RProxy(RProxy)
   , kind RowList
   )
@@ -132,12 +138,7 @@ class REqual
   (r :: # Type)
   | l -> r
   where
-  requal
-    :: RowToList r l
-    => RLProxy l
-    -> f r
-    -> f r
-    -> Boolean
+  requal :: RowToList r l => RLProxy l -> f r -> f r -> Boolean
 
 instance requalRecord :: Record.EqualFields l r => REqual Record l r where
   requal _ = Record.equal
@@ -160,13 +161,7 @@ class RGet
   (r :: # Type)
   | l -> r
   where
-  rget
-    :: forall r' v
-     . Cons s v r' r
-    => RLProxy l
-    -> g s
-    -> f r
-    -> v
+  rget :: forall r' v . Cons s v r' r => RLProxy l -> g s -> f r -> v
 
 instance rgetRecord :: IsSymbol s => RGet Record SProxy s l r where
   rget _ = Record.get
@@ -386,6 +381,19 @@ instance rsetVariant :: IsSymbol s => RSet Variant SProxy s l0 r0 l1 r1 where
       (Variant.inj s <<< const v)
       unsafeCoerce
 
+class RSingleton
+  (f :: # Type -> Type)
+  (g :: Symbol -> Type)
+  (s :: Symbol)
+  where
+  rsingleton :: forall r v. ListToRow (Cons s v Nil) r => g s -> v -> f r
+
+instance rsingletonRecord
+  :: IsSymbol s
+  => RSingleton Record SProxy s
+  where
+  rsingleton = Record.singleton
+
 class RUnion
   (f  :: # Type -> Type)
   (l0 :: RowList)
@@ -412,3 +420,165 @@ instance runionRecord :: RUnion Record l0 r0 l1 r1 l2 r2 where
 
 instance runionRProxy :: RUnion RProxy l0 r0 l1 r1 l2 r2 where
   runion _ _ _ _ _ = RProxy
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- module Record.Builder
+--   ( Builder
+--   , build
+--   , insert
+--   , modify
+--   , delete
+--   , rename
+--   , merge
+--   , union
+--   , disjointUnion
+--   , nub
+--   ) where
+--
+-- import Prelude
+--
+-- import Data.Function.Uncurried (runFn2)
+-- import Data.Symbol (class IsSymbol, SProxy, reflectSymbol)
+-- import Record.Unsafe.Union (unsafeUnionFn)
+-- import Type.Row as Row
+-- import Unsafe.Coerce (unsafeCoerce)
+--
+-- foreign import copyRecord :: forall r1. Record r1 -> Record r1
+-- foreign import unsafeInsert :: forall a r1 r2. String -> a -> Record r1 -> Record r2
+-- foreign import unsafeModify :: forall a b r1 r2. String -> (a -> b) -> Record r1 -> Record r2
+-- foreign import unsafeDelete :: forall r1 r2. String -> Record r1 -> Record r2
+-- foreign import unsafeRename :: forall r1 r2. String -> String -> Record r1 -> Record r2
+--
+-- -- | A `Builder` can be used to `build` a record by incrementally adding
+-- -- | fields in-place, instead of using `insert` and repeatedly generating new
+-- -- | immutable records which need to be garbage collected.
+-- -- |
+-- -- | The mutations accumulated in a `Builder` are safe because intermediate states can't be
+-- -- | observed. These mutations, then, are performed all-at-once in the `build` function.
+-- -- |
+-- -- | The `Category` instance for `Builder` can be used to compose builders.
+-- -- |
+-- -- | For example:
+-- -- |
+-- -- | ```purescript
+-- -- | build (insert x 42 >>> insert y "testing") {} :: { x :: Int, y :: String }
+-- -- | ```
+-- newtype Builder a b = Builder (a -> b)
+--
+-- -- | Build a record, starting from some other record.
+-- build :: forall r1 r2. Builder (Record r1) (Record r2) -> Record r1 -> Record r2
+-- build (Builder b) r1 = b (copyRecord r1)
+--
+-- derive newtype instance semigroupoidBuilder :: Semigroupoid Builder
+-- derive newtype instance categoryBuilder :: Category Builder
+--
+-- -- | Build by inserting a new field.
+-- insert
+--   :: forall l a r1 r2
+--    . Row.Cons l a r1 r2
+--   => Row.Lacks l r1
+--   => IsSymbol l
+--   => SProxy l
+--   -> a
+--   -> Builder (Record r1) (Record r2)
+-- insert l a = Builder \r1 -> unsafeInsert (reflectSymbol l) a r1
+--
+-- -- | Build by modifying an existing field.
+-- modify
+--   :: forall l a b r r1 r2
+--    . Row.Cons l a r r1
+--   => Row.Cons l b r r2
+--   => IsSymbol l
+--   => SProxy l
+--   -> (a -> b)
+--   -> Builder (Record r1) (Record r2)
+-- modify l f = Builder \r1 -> unsafeModify (reflectSymbol l) f r1
+--
+-- -- | Build by deleting an existing field.
+-- delete
+--   :: forall l a r1 r2
+--    . IsSymbol l
+--    => Row.Lacks l r1
+--    => Row.Cons l a r1 r2
+--    => SProxy l
+--    -> Builder (Record r2) (Record r1)
+-- delete l = Builder \r2 -> unsafeDelete (reflectSymbol l) r2
+--
+-- -- | Build by renaming an existing field.
+-- rename :: forall l1 l2 a r1 r2 r3
+--    . IsSymbol l1
+--   => IsSymbol l2
+--   => Row.Cons l1 a r2 r1
+--   => Row.Lacks l1 r2
+--   => Row.Cons l2 a r2 r3
+--   => Row.Lacks l2 r2
+--   => SProxy l1
+--   -> SProxy l2
+--   -> Builder (Record r1) (Record r3)
+-- rename l1 l2 = Builder \r1 -> unsafeRename (reflectSymbol l1) (reflectSymbol l2) r1
+--
+-- -- | Build by merging existing fields from another record.
+-- merge
+--   :: forall r1 r2 r3 r4
+--    . Row.Union r1 r2 r3
+--   => Row.Nub r3 r4
+--   => Record r2
+--   -> Builder (Record r1) (Record r4)
+-- merge r2 = Builder \r1 -> runFn2 unsafeUnionFn r1 r2
+--
+-- -- | Build by merging existing fields from another record. Unlike `merge`,
+-- -- | this does not remove duplicate labels from the resulting record type.
+-- -- | This can result in better inference for some pipelines, deferring the
+-- -- | need for a `Nub` constraint.
+-- union
+--   :: forall r1 r2 r3
+--    . Row.Union r1 r2 r3
+--   => Record r2
+--   -> Builder (Record r1) (Record r3)
+-- union r2 = Builder \r1 -> runFn2 unsafeUnionFn r1 r2
+--
+-- -- | Build by merging some disjoint set of fields from another record.
+-- disjointUnion
+--   :: forall r1 r2 r3
+--    . Row.Union r1 r2 r3
+--   => Row.Nub r3 r3
+--   => Record r1
+--   -> Builder (Record r2) (Record r3)
+-- disjointUnion r1 = Builder \r2 -> runFn2 unsafeUnionFn r1 r2
+--
+-- -- | A coercion which removes duplicate labels from a record's type.
+-- nub
+--   :: forall r1 r2
+--    . Row.Nub r1 r2
+--   => Builder (Record r1) (Record r2)
+-- nub = Builder unsafeCoerce
---
+++ src/Record/EmbedIntoFunction.purs
@@ -0,0 +1,51 @@
+module Record.Extra.EmbedIntoFunction where
+
+import Prelude (identity)
+import Data.Symbol (class IsSymbol, SProxy(SProxy))
+import Type.Data.RowList (RLProxy(RLProxy)) -- Argonaut dependency
+import Type.Row
+  ( class Cons
+  , class Lacks
+  , Cons
+  , Nil
+  , kind RowList
+  )
+import Unsafe.Coerce (unsafeCoerce)
+import Data.RecordLike (class RGet, class RInsert, rget, rinsert)
+
+class EmbedIntoFunction
+  (l0 :: RowList)
+  (r0 :: # Type)
+  (l1 :: RowList)
+  (r1 :: # Type)
+  (r2 :: # Type)
+  | l0 -> r0
+  , l1 -> r1
+  , l0 l1 -> r2
+  where
+  embed :: RLProxy l0 -> RLProxy l1 -> Record r0 -> Record r1 -> Record r2
+
+instance embedIntoFunction_Nil :: EmbedIntoFunction Nil () l r r where
+  embed _ _ _ = identity
+
+instance embedIntoFunction_Cons
+  :: ( Cons s v r0' r0
+     , Cons s v r2' r2
+     , EmbedIntoFunction l0' r0' l1 r1 r2'
+     , IsSymbol s
+     , Lacks s r0'
+     , Lacks s r2'
+     , RGet Record SProxy s l0 r0
+     , RInsert Record SProxy s l2' r2' l2 r2
+     )
+  => EmbedIntoFunction (Cons s v l0') r0 l1 r1 r2
+  where
+  embed l0 l1 record0 src =
+      rinsert l2' l2 s (rget l0 s record0) (embed l0' l1 record0' src)
+    where
+    l0' = (RLProxy :: RLProxy l0')
+    l2' = (RLProxy :: RLProxy l2')
+    l2 = (RLProxy :: RLProxy l2)
+    s = (SProxy :: SProxy s)
+    record0' :: Record r0'
+    record0' = unsafeCoerce record0
