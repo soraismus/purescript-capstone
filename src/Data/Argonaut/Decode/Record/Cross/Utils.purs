fce9eb6cb6f12721f98c907774f12a213667442e
matthew.hilty <hilty.matthew@gmail.com>
2019-06-02 14:51:28 -0400

    snapshot

--- src/Data/Argonaut/Decode/Record/Cross/Utils.purs
+++ src/Data/Argonaut/Decode/Record/Cross/Utils.purs
@@ -13,13 +13,14 @@ import Data.Argonaut.Decode.Record.Cross.Class
 import Data.Argonaut.Decode.Record.Utils (reportJson, reportObject)
 import Data.Status (class Status, report)
 import Foreign.Object (Object)
+import Record.Builder (Builder, build)
 import Type.Data.RowList (RLProxy(RLProxy)) -- Argonaut dependency
 import Type.Row (class RowToList)
 
 decodeJsonWith
   :: forall f l0 l2 r0 r2 r3
    . Bind f
-  => D.DecodeJsonWith Function f Record l0 r0 l2 r2 r3 (Record r2)
+  => D.DecodeJsonWith Builder f Record l0 r0 l2 r2 r3 (Record r2)
   => GDecodeJson r2 l2
   => RowToList r0 l0
   => RowToList r2 l2
@@ -32,11 +33,11 @@ decodeJsonWith decoderRecord = reportJson go
   go :: Object Json -> f (Record r3)
   go object = do
     (record2 :: Record r2) <- reportObject object
-    (addFields0 :: Record r2 -> Record r3) <-
+    (addFields0 :: Builder (Record r2) (Record r3)) <-
       D.decodeJsonWith
         (RLProxy :: RLProxy l0)
         (RLProxy :: RLProxy l2)
         decoderRecord
         object
         record2
-    report $ addFields0 record2
+    report $ build addFields0 record2
