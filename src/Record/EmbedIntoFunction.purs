fcfc9747e9d98e363ec76bb73a37fe1e781a94b8
matthew.hilty <hilty.matthew@gmail.com>
2019-06-03 00:08:45 -0400

    snapshot

--- src/Record/EmbedIntoFunction.purs
+++ src/Record/EmbedIntoFunction.purs
@@ -1,13 +1,13 @@
-module Record.Extra.GAlter where
+module Record.Extra.GMapRecord where
 
 import Prelude (identity)
 import Data.Symbol (class IsSymbol, SProxy(SProxy))
 import Type.Data.RowList (RLProxy(RLProxy)) -- Argonaut dependency
-import Type.Row (class Cons, class Lacks, Cons, Nil, kind RowList)
+import Type.Row (class Cons, Cons, Nil, kind RowList)
 import Unsafe.Coerce (unsafeCoerce)
-import Data.RecordLike (class RGet, class RInsert, class RModify, rget, rinsert, rmodify)
+import Data.RecordLike (class RGet, class RModify, rget, rmodify)
 
-class GAlter
+class GMapRecord
   (l0 :: RowList)
   (r0 :: # Type)
   (l1 :: RowList)
@@ -17,24 +17,24 @@ class GAlter
   , l1 -> r1
   , l0 l1 -> r2
   where
-  gAlter :: RLProxy l0 -> RLProxy l1 -> Record r0 -> Record r1 -> Record r2
+  gMapRecord :: RLProxy l0 -> RLProxy l1 -> Record r0 -> Record r1 -> Record r2
 
-instance gAlter_Nil :: GAlter Nil () l r r where
-  gAlter _ _ _ = identity
+instance gMapRecord_Nil :: GMapRecord Nil () l r r where
+  gMapRecord _ _ _ = identity
 
-instance gAlter_Cons_RModify
+instance gMapRecord_Cons
   :: ( Cons s (va -> vb) r0' r0
      , Cons s va r r2'
      , Cons s vb r r2
-     , GAlter l0' r0' l1 r1 r2'
+     , GMapRecord l0' r0' l1 r1 r2'
      , IsSymbol s
      , RGet Record SProxy s l0 r0
      , RModify Function Record SProxy s l2' r2' l2 r2
      )
-  => GAlter (Cons s v l0') r0 l1 r1 r2
+  => GMapRecord (Cons s v l0') r0 l1 r1 r2
   where
-  gAlter l0 l1 record0 src =
-      rmodify l2' l2 s (rget l0 s record0) (gAlter l0' l1 record0' src)
+  gMapRecord l0 l1 record0 src =
+      rmodify l2' l2 s (rget l0 s record0) (gMapRecord l0' l1 record0' src)
     where
     l0' = (RLProxy :: RLProxy l0')
     l2' = (RLProxy :: RLProxy l2')
@@ -42,70 +42,3 @@ instance gAlter_Cons_RModify
     s = (SProxy :: SProxy s)
     record0' :: Record r0'
     record0' = unsafeCoerce record0
-
-else instance gAlter_Cons_RInsert
-  :: ( Cons s v r0' r0
-     , Cons s v r2' r2
-     , GAlter l0' r0' l1 r1 r2'
-     , IsSymbol s
-     , Lacks s r2'
-     , RGet Record SProxy s l0 r0
-     , RInsert Function Record SProxy s l2' r2' l2 r2
-     )
-  => GAlter (Cons s v l0') r0 l1 r1 r2
-  where
-  gAlter l0 l1 record0 src =
-      rinsert l2' l2 s (rget l0 s record0) (gAlter l0' l1 record0' src)
-    where
-    l0' = (RLProxy :: RLProxy l0')
-    l2' = (RLProxy :: RLProxy l2')
-    l2 = (RLProxy :: RLProxy l2)
-    s = (SProxy :: SProxy s)
-    record0' :: Record r0'
-    record0' = unsafeCoerce record0
-
-class RTransform
-  (a  :: Type)
-  (p  :: Type -> Type -> Type)
-  (f  :: # Type -> Type)
-  (g  :: Symbol -> Type)
-  (s  :: Symbol)
-  (v  :: Type)
-  (l0 :: RowList)
-  (r0 :: # Type)
-  (l1 :: RowList)
-  (r1 :: # Type)
-  | l0 -> r0
-  , l1 -> r1
-  where
-  rtransform
-    :: a
-    -> RLProxy l0
-    -> RLProxy l1
-    -> g s
-    -> v
-    -> p (f r0) (f r1)
-
-data RInsert_ = RInsert_
-data RModify_ = RModify_
-
-instance rtransformRInsert
-  :: ( RInsert p f g s l0 r0 l1 r1
-     , Cons s v r0 r1
-     , Lacks s r0
-     , IsSymbol s
-     )
-  => RTransform RInsert_ p f g s v l0 r0 l1 r1
-  where
-  rtransform _ = rinsert
-
-instance rtransformRModify
-  :: ( RModify p f g s l0 r0 l1 r1
-     , Cons s v0 r r0
-     , Cons s v1 r r1
-     , IsSymbol s
-     )
-  => RTransform RModify_ p f g s (v0 -> v1) l0 r0 l1 r1
-  where
-  rtransform _ = rmodify
-
