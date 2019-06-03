278fdad2f67531a7aee8ae3f007538ea2d9f8b00
matthew.hilty <hilty.matthew@gmail.com>
2019-06-03 00:01:33 -0400

    snapshot

--- src/Record/EmbedIntoFunction.purs
+++ src/Record/EmbedIntoFunction.purs
@@ -1,4 +1,4 @@
-module Record.Extra.EmbedIntoFunction where
+module Record.Extra.GAlter where
 
 import Prelude (identity)
 import Data.Symbol (class IsSymbol, SProxy(SProxy))
@@ -7,7 +7,7 @@ import Type.Row (class Cons, class Lacks, Cons, Nil, kind RowList)
 import Unsafe.Coerce (unsafeCoerce)
 import Data.RecordLike (class RGet, class RInsert, class RModify, rget, rinsert, rmodify)
 
-class EmbedIntoFunction
+class GAlter
   (l0 :: RowList)
   (r0 :: # Type)
   (l1 :: RowList)
@@ -17,10 +17,52 @@ class EmbedIntoFunction
   , l1 -> r1
   , l0 l1 -> r2
   where
-  embed :: RLProxy l0 -> RLProxy l1 -> Record r0 -> Record r1 -> Record r2
+  gAlter :: RLProxy l0 -> RLProxy l1 -> Record r0 -> Record r1 -> Record r2
 
-instance embedIntoFunction_Nil :: EmbedIntoFunction Nil () l r r where
-  embed _ _ _ = identity
+instance gAlter_Nil :: GAlter Nil () l r r where
+  gAlter _ _ _ = identity
+
+instance gAlter_Cons_RModify
+  :: ( Cons s (va -> vb) r0' r0
+     , Cons s va r r2'
+     , Cons s vb r r2
+     , GAlter l0' r0' l1 r1 r2'
+     , IsSymbol s
+     , RGet Record SProxy s l0 r0
+     , RModify Function Record SProxy s l2' r2' l2 r2
+     )
+  => GAlter (Cons s v l0') r0 l1 r1 r2
+  where
+  gAlter l0 l1 record0 src =
+      rmodify l2' l2 s (rget l0 s record0) (gAlter l0' l1 record0' src)
+    where
+    l0' = (RLProxy :: RLProxy l0')
+    l2' = (RLProxy :: RLProxy l2')
+    l2 = (RLProxy :: RLProxy l2)
+    s = (SProxy :: SProxy s)
+    record0' :: Record r0'
+    record0' = unsafeCoerce record0
+
+else instance gAlter_Cons_RInsert
+  :: ( Cons s v r0' r0
+     , Cons s v r2' r2
+     , GAlter l0' r0' l1 r1 r2'
+     , IsSymbol s
+     , Lacks s r2'
+     , RGet Record SProxy s l0 r0
+     , RInsert Function Record SProxy s l2' r2' l2 r2
+     )
+  => GAlter (Cons s v l0') r0 l1 r1 r2
+  where
+  gAlter l0 l1 record0 src =
+      rinsert l2' l2 s (rget l0 s record0) (gAlter l0' l1 record0' src)
+    where
+    l0' = (RLProxy :: RLProxy l0')
+    l2' = (RLProxy :: RLProxy l2')
+    l2 = (RLProxy :: RLProxy l2)
+    s = (SProxy :: SProxy s)
+    record0' :: Record r0'
+    record0' = unsafeCoerce record0
 
 class RTransform
   (a  :: Type)
@@ -67,36 +109,3 @@ instance rtransformRModify
   where
   rtransform _ = rmodify
 
-instance embedIntoFunction_Cons
-  :: (
-
---      , RInsert Function Record SProxy s l2' r2' l2 r2
---        Cons s v r0' r0
---      , Cons s v r2' r2
---      , Lacks s r0'
---      -, Lacks s r2'
-
---      , RModify Function Record SProxy s l2' r2' l2 r2
---        Cons s (va -> vb) r0' r0
---      , Cons s va r r2'
---      , Cons s vb r r2
-
-       Cons s (va -> vb) r0' r0
-     , Cons s va r r2'
-     , Cons s vb r r2
-     , EmbedIntoFunction l0' r0' l1 r1 r2'
-     , IsSymbol s
-     , RGet Record SProxy s l0 r0
-     , RModify Function Record SProxy s l2' r2' l2 r2
-     )
-  => EmbedIntoFunction (Cons s v l0') r0 l1 r1 r2
-  where
-  embed l0 l1 record0 src =
-      rmodify l2' l2 s (rget l0 s record0) (embed l0' l1 record0' src)
-    where
-    l0' = (RLProxy :: RLProxy l0')
-    l2' = (RLProxy :: RLProxy l2')
-    l2 = (RLProxy :: RLProxy l2)
-    s = (SProxy :: SProxy s)
-    record0' :: Record r0'
-    record0' = unsafeCoerce record0
