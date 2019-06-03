2048fdbc24ba34b7731e5eab8a4d40a5ea45f8c9
matthew.hilty <hilty.matthew@gmail.com>
2019-06-02 23:05:23 -0400

    snapshot

--- src/Record/EmbedIntoFunction.purs
+++ src/Record/EmbedIntoFunction.purs
@@ -3,15 +3,9 @@ module Record.Extra.EmbedIntoFunction where
 import Prelude (identity)
 import Data.Symbol (class IsSymbol, SProxy(SProxy))
 import Type.Data.RowList (RLProxy(RLProxy)) -- Argonaut dependency
-import Type.Row
-  ( class Cons
-  , class Lacks
-  , Cons
-  , Nil
-  , kind RowList
-  )
+import Type.Row (class Cons, Cons, Nil, kind RowList)
 import Unsafe.Coerce (unsafeCoerce)
-import Data.RecordLike (class RGet, class RInsert, rget, rinsert)
+import Data.RecordLike (class RGet, class RModify, rget, rmodify)
 
 class EmbedIntoFunction
   (l0 :: RowList)
@@ -29,19 +23,30 @@ instance embedIntoFunction_Nil :: EmbedIntoFunction Nil () l r r where
   embed _ _ _ = identity
 
 instance embedIntoFunction_Cons
-  :: ( Cons s v r0' r0
-     , Cons s v r2' r2
+  :: (
+--      , RInsert Function Record SProxy s l2' r2' l2 r2
+--        Cons s v r0' r0
+--      , Cons s v r2' r2
+--      , Lacks s r0'
+--      -, Lacks s r2'
+
+--      , RModify Function Record SProxy s l2' r2' l2 r2
+--        Cons s (va -> vb) r0' r0
+--      , Cons s va r r2'
+--      , Cons s vb r r2
+
+       Cons s (va -> vb) r0' r0
+     , Cons s va r r2'
+     , Cons s vb r r2
      , EmbedIntoFunction l0' r0' l1 r1 r2'
      , IsSymbol s
-     , Lacks s r0'
-     , Lacks s r2'
      , RGet Record SProxy s l0 r0
-     , RInsert Function Record SProxy s l2' r2' l2 r2
+     , RModify Function Record SProxy s l2' r2' l2 r2
      )
   => EmbedIntoFunction (Cons s v l0') r0 l1 r1 r2
   where
   embed l0 l1 record0 src =
-      rinsert l2' l2 s (rget l0 s record0) (embed l0' l1 record0' src)
+      rmodify l2' l2 s (rget l0 s record0) (embed l0' l1 record0' src)
     where
     l0' = (RLProxy :: RLProxy l0')
     l2' = (RLProxy :: RLProxy l2')
