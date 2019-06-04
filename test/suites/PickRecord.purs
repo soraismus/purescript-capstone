0c2c3e969ca5d84d215661695881cc066e5738b2
matthew.hilty <hilty.matthew@gmail.com>
2019-06-04 19:40:47 -0400

    snapshot

--- test/suites/PickRecord.purs
+++ test/suites/PickRecord.purs
@@ -2,7 +2,7 @@ module Test.Suites.PickRecord
   ( suites
   ) where
 
-import Prelude (discard)
+import Prelude (discard, pure, unit, ($))
 
 import Record.Builder (build)
 import Record.Extra.PickRecord (pickRecord)
@@ -33,6 +33,28 @@ suites =
         let input = { a0: 0, a1: 1, a2: 2 }
         build pickRecord input `shouldEqual` { a0: 0, a2: 2 }
 
+      test "#5 -- Should Not Compile" $ pure unit
+--         let input = { a0: 0, a1: 1, a2: 2 }
+--         build (pickRecord <<< pickRecord) input
+--           `shouldEqual`
+--           { a0: 0, a2: 2 }
+
+      test "#6 -- Should Not Compile" $ pure unit
+--         let input = { a0: 0, a1: 1, a2: 2 }
+--         build (pickRecord <<< identity <<< pickRecord) input
+--           `shouldEqual`
+--           { a0: 0, a2: 2 }
+
+      test "#7 -- Should Not Compile" $ pure unit
+--         let input = {}
+--         build pickRecord input `shouldEqual` { a0: 0 }
+
+      test "#8" do
+        let input = { a0: 0, a1: 1, a2: 2, a3: 3, a4: 4, a5: 5 }
+        build pickRecord input
+          `shouldEqual`
+          { a5: 5, a3: 3, a1: 1, a0: 0, a2: 2, a4: 4 }
+
     suite "Function" do
       test "#0" do
         let input = {}
@@ -53,3 +75,23 @@ suites =
       test "#4" do
         let input = { a0: 0, a1: 1, a2: 2 }
         pickRecord input `shouldEqual` { a0: 0, a2: 2 }
+
+      test "#5 -- Should Not Compile" $ pure unit
+--         let input = { a0: 0, a1: 1, a2: 2 }
+--         (pickRecord <<< pickRecord) input `shouldEqual` { a0: 0, a2: 2 }
+
+      test "#6 -- Should Not Compile" $ pure unit
+--         let input = { a0: 0, a1: 1, a2: 2 }
+--         (pickRecord <<< identity <<< pickRecord) input
+--           `shouldEqual`
+--           { a0: 0, a2: 2 }
+
+      test "#7 -- Should Not Compile" $ pure unit
+--         let input = {}
+--         pickRecord input `shouldEqual` { a0: 0 }
+
+      test "#8" do
+        let input = { a0: 0, a1: 1, a2: 2, a3: 3, a4: 4, a5: 5 }
+        pickRecord input
+          `shouldEqual`
+          { a5: 5, a3: 3, a1: 1, a0: 0, a2: 2, a4: 4 }
